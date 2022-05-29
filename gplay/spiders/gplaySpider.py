48141079f0ead6058935f7d0e07d186a16c2f0b3
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2022-05-30 01:03:38 +0300

    Refactor code

--- gplay/spiders/gplaySpider.py
+++ gplay/spiders/gplaySpider.py
@@ -16,27 +16,25 @@ class GplaySpider(scrapy.Spider):
             yield response.follow(category, callback=self.parse_products)
 
     def parse_products(self, response):
-        products = response.xpath("//div[@class='product-item']")
+        products = response.xpath("//div[@class='product-item']/a[@class='product-name']/@href").getall()
         for product in products:
-            url = product.xpath("./a[@class='product-name']/@href").get()
-            yield scrapy.Request(url, callback=self.parse_single_product)
+            yield scrapy.Request(product, callback=self.parse_single_product)
 
         next_page = response.xpath("(//ul[@role='navigation']/li[@class='page-item'])[last()]/a/@href").get()
         if next_page is not None:
             yield scrapy.Request(url=next_page, callback=self.parse_products)
 
     def parse_single_product(self, response):
-        item = GplayItem()
-
         price = response.xpath("//div[@class='price']/div[contains(@class, 'normal-price')]/price/@*").get()
         price_format = "{:.1f}".format(float(price))
         final_price = float(price_format)
-
+        
+        item = GplayItem()
+        item['product_number'] = response.xpath("//div[@class='col-md-6 product-ref-number']/strong/text()").get()
         item['category'] = response.xpath("(//div[@class='path py-3']/a)[2]/text()").get()
         item['subcategory'] = response.xpath("(//div[@class='path py-3']/a)[3]/text()").get()
         item['title'] = response.xpath("//h1[@class='large-title']/text()").get()
         item['subtitle'] = response.xpath("//h2[@class='product-subtitle ']/text()").get()
-        item['product_number'] = response.xpath("//div[@class='col-md-6 product-ref-number']/strong/text()").get()
         item['price'] = final_price
 
         yield item
